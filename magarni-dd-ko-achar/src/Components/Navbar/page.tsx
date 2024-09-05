ec1da3adeb354b7653a48a80db48ab280a8c80b0
Shankar Magar <shankaralemagar91@gmail.com>
2024-09-05 16:19:40 -0400

    Fixed naming

--- magarni-dd-ko-achar/src/Components/Navbar/page.tsx
+++ magarni-dd-ko-achar/src/Components/Navbar/page.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import logo from '../../images/logoAchar.png'
-import cart from '../../images/Cart.png'
+import cart from '../../images/cart.png'
 
 export default function Navbar() {
   return (
